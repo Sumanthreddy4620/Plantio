b0553e326c41f4b408c66e32b43a8ac5d88d8fde
Sumanth Reddy <k.sumanthreddy4620@gmail.com>
2026-07-25 20:23:38 +0530

    Remove horizontal scrollbar indicator on category pill bar and update species count text

--- src/components/Plants.jsx
+++ src/components/Plants.jsx
@@ -37,7 +37,7 @@ export default function Plants() {
             />
             <input type="submit" className="but" value="🔍︎" />
           </form>
-          <span>Search from 32+ plant species</span>
+          <span>Search from 65+ plant species</span>
         </div>
       </div>
 
